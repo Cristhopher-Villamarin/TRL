4b9787b850f05fd365e657ff28e474b8ae772eba
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2025-11-21 21:28:16 -0500

    update backend structure with springboot

---
+++ Backend_TRL/src/main/java/trl/TRL/TrlApplication.java
@@ -0,0 +1,13 @@
+package trl.TRL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrlApplication.class, args);
+	}
+
+}
---
+++ Backend_TRL/src/test/java/trl/TRL/TrlApplicationTests.java
@@ -0,0 +1,13 @@
+package trl.TRL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
